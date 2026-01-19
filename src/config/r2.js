719b9166f1981633e3da9e39544f4f5794caa802
rebuyapp-ui <thethoufik46>
2026-01-19 11:58:10 +0530

    Fix server entry file location

--- src/config/r2.js
+++ src/config/r2.js
@@ -1,8 +1,13 @@
+
 import { S3Client } from "@aws-sdk/client-s3";
 
 const r2 = new S3Client({
   region: "auto",
+
   endpoint: `https://${process.env.CF_ACCOUNT_ID}.r2.cloudflarestorage.com`,
+
+  forcePathStyle: true, // 🔥 THIS LINE FIXES SSL
+
   credentials: {
     accessKeyId: process.env.CF_ACCESS_KEY,
     secretAccessKey: process.env.CF_SECRET_KEY,

dc9ea2d567c57797c654c1cca639cc00695a90ed
rebuyapp-ui <thethoufik46>
2026-01-19 12:55:18 +0530

    Fix server entry file location

--- src/config/r2.js
+++ src/config/r2.js
@@ -1,8 +1,8 @@
 import { S3Client } from "@aws-sdk/client-s3";
 
 const r2 = new S3Client({
-  region: "auto", // 🔥 MUST
-  endpoint: process.env.R2_ENDPOINT, // 🔥 MUST
+  region: "auto",
+  endpoint: process.env.R2_ENDPOINT,
   credentials: {
     accessKeyId: process.env.R2_ACCESS_KEY,
     secretAccessKey: process.env.R2_SECRET_KEY,

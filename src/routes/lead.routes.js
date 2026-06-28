3066953ad64f1be09c8a17712647acaf99c696fe
rebuyapp-ui <thethoufik46@gmail.com>
2026-06-28 20:34:52 +0530

    Backend setup complete

--- src/routes/lead.routes.js
+++ src/routes/lead.routes.js
@@ -8,31 +8,46 @@ import {
   deleteLead,
 } from "../controllers/lead.controller.js";
 
+import uploadLead from "../middleware/uploadLead.js";
+
 const router = express.Router();
 
 /* =====================================================
    ADD LEAD
 ===================================================== */
-router.post("/add", addLead);
+
+router.post(
+  "/add",
+  uploadLead.single("audio"),
+  addLead
+);
 
 /* =====================================================
    GET ALL LEADS
 ===================================================== */
+
 router.get("/", getLeads);
 
 /* =====================================================
    GET SINGLE LEAD
 ===================================================== */
+
 router.get("/:id", getLead);
 
 /* =====================================================
    UPDATE LEAD
 ===================================================== */
-router.put("/:id", updateLead);
+
+router.put(
+  "/:id",
+  uploadLead.single("audio"),
+  updateLead
+);
 
 /* =====================================================
    DELETE LEAD
 ===================================================== */
+
 router.delete("/:id", deleteLead);
 
 export default router;
